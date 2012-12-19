114e8c78cdebcddf77e9316d5b586da0b86b8abf
CHAOSFISCH <chaosking0912@gmail.com>
2012-12-19 12:14:52 +0100

    Initial gradle update 3 commit

---
+++ src/main/java/org/chaosfisch/util/io/Request.java
@@ -0,0 +1,135 @@
+package org.chaosfisch.util.io;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.client.methods.HttpOptions;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpTrace;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.params.HttpParams;
+
+public class Request
+{
+	public enum Method
+	{
+		GET, POST, PUT, DELETE, HEAD, OPTIONS, TRACE, CONNECT
+	}
+
+	private HttpUriRequest		httpRequest;
+	private HttpURLConnection	urlConnection;
+
+	public static class Builder
+	{
+		private final String		url;
+		private HttpParams			params;
+		private final Method		method;
+		private Map<String, String>	headers;
+		private HttpEntity			entity;
+
+		public Builder(final String url, final Method method)
+		{
+			this.url = url;
+			this.method = method;
+		}
+
+		public Builder params(final HttpParams params)
+		{
+			this.params = params;
+			return this;
+		}
+
+		public Builder headers(final Map<String, String> headers)
+		{
+			this.headers = headers;
+			return this;
+		}
+
+		public Builder entity(final HttpEntity entity)
+		{
+			this.entity = entity;
+			return this;
+		}
+
+		public HttpUriRequest buildHttpUriRequest()
+		{
+			return new Request(this).getHttpUriRequest();
+		}
+
+		public HttpURLConnection buildHttpUrlConnection() throws IOException
+		{
+			return new Request(this, false).getUrlConnection();
+		}
+	}
+
+	private Request(final Builder builder)
+	{
+		switch (builder.method)
+		{
+			case DELETE:
+				httpRequest = new HttpDelete(builder.url);
+				break;
+			case HEAD:
+				httpRequest = new HttpHead(builder.url);
+				break;
+			case OPTIONS:
+				httpRequest = new HttpOptions(builder.url);
+				break;
+			case POST:
+				httpRequest = new HttpPost(builder.url);
+				((HttpEntityEnclosingRequestBase) httpRequest).setEntity(builder.entity);
+				break;
+			case PUT:
+				httpRequest = new HttpPut(builder.url);
+				((HttpEntityEnclosingRequestBase) httpRequest).setEntity(builder.entity);
+				break;
+			case TRACE:
+				httpRequest = new HttpTrace(builder.url);
+				break;
+			default:
+			case GET:
+				httpRequest = new HttpGet(builder.url);
+				break;
+		}
+
+		if (builder.headers != null)
+		{
+			for (final Entry<String, String> entry : builder.headers.entrySet())
+			{
+				httpRequest.addHeader(entry.getKey(), entry.getValue());
+			}
+		}
+		httpRequest.setParams(builder.params != null ? builder.params : httpRequest.getParams());
+	}
+
+	public Request(final Builder builder, final boolean b) throws IOException
+	{
+		final URL url = new URL(builder.url);
+		urlConnection = (HttpURLConnection) url.openConnection();
+		urlConnection.setRequestMethod(builder.method.name());
+
+		for (final Entry<String, String> entry : builder.headers.entrySet())
+		{
+			urlConnection.setRequestProperty(entry.getKey(), entry.getValue());
+		}
+	}
+
+	private HttpUriRequest getHttpUriRequest()
+	{
+		return httpRequest;
+	}
+
+	private HttpURLConnection getUrlConnection()
+	{
+		return urlConnection;
+	}
+}
---
+++ src/main/java/org/chaosfisch/util/io/ThrottledOutputStream.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2012, Dennis Fischer
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.chaosfisch.util.io;
+
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ThrottledOutputStream extends FilterOutputStream
+{
+	private long			bytes;
+	private final long		start;
+	private final Throttle	throttle;
+
+	// / Constructor.
+	public ThrottledOutputStream(final OutputStream out, final Throttle throttle)
+	{
+		super(out);
+		this.throttle = throttle;
+		bytes = 0;
+		start = System.currentTimeMillis();
+	}
+
+	private final byte[]	oneByte	= new byte[1];
+
+	// / Writes a byte. This method will block until the byte is actually
+	// written.
+	// @param b the byte to be written
+	// @exception IOException if an I/O error has occurred
+	@Override
+	public void write(final int b) throws IOException
+	{
+		oneByte[0] = (byte) b;
+		write(oneByte, 0, 1);
+	}
+
+	// / Writes a subarray of bytes.
+	// @param b the data to be written
+	// @param off the start offset in the data
+	// @param len the number of bytes that are written
+	// @exception IOException if an I/O error has occurred
+	@Override
+	public void write(final byte[] b, final int off, final int len) throws IOException
+	{
+		// Check the throttle.
+		bytes += len;
+		final long elapsed = Math.max(System.currentTimeMillis() - start, 1);
+
+		final long bps = (bytes * 1000L) / elapsed;
+		if ((throttle.maxBps.get() != 0) && (bps > throttle.maxBps.multiply(1000).get()))
+		{
+			// Oops, sending too fast.
+			final long wakeElapsed = (bytes * 1000L) / throttle.maxBps.multiply(1000).get();
+			try
+			{
+				Thread.sleep(wakeElapsed - elapsed);
+			} catch (final InterruptedException ignored)
+			{}
+		}
+
+		// Write the bytes.
+		out.write(b, off, len);
+	}
+}
