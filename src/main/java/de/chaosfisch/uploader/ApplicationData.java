64ec1f0866d70e8e53279768cf12ad541610dd4c
Dennis Fischer <chaosking0912@gmail.com>
2013-09-06 14:29:06 +0200

    v3.1.0.3 release information.

--- src/main/java/de/chaosfisch/uploader/ApplicationData.java
+++ src/main/java/de/chaosfisch/uploader/ApplicationData.java
@@ -13,8 +13,8 @@ package de.chaosfisch.uploader;
 import com.sun.javafx.PlatformUtil;
 
 public final class ApplicationData {
-	public static final String VERSION = "3.1.0.2";
-	public static final int    RELEASE = 14;
+	public static final String VERSION = "3.1.0.3";
+	public static final int    RELEASE = 15;
 	public static final String BASEURL = "http://youtubeuploader.square7.ch/nightly/jupidator/update.xml";
 	public static final String HOME;
 	public static final String DATA_DIR;
