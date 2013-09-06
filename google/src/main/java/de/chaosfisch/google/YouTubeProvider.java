6964b54e2b85599abd855f8e6ce97ba208959801
Dennis Fischer <chaosking0912@gmail.com>
2013-09-06 08:35:31 +0200

    Added logging option for tokenResponse

--- google/src/main/java/de/chaosfisch/google/YouTubeProvider.java
+++ google/src/main/java/de/chaosfisch/google/YouTubeProvider.java
@@ -11,6 +11,9 @@
 package de.chaosfisch.google;
 
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.auth.oauth2.CredentialRefreshListener;
+import com.google.api.client.auth.oauth2.TokenErrorResponse;
+import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.*;
 import com.google.api.client.json.JsonFactory;
@@ -19,6 +22,8 @@ import com.google.api.services.youtube.YouTube;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import de.chaosfisch.google.account.Account;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -29,6 +34,7 @@ public class YouTubeProvider implements Provider<YouTube> {
 	private final Map<Account, YouTube> services = new HashMap<>(1);
 	private final HttpTransport httpTransport;
 	private final JsonFactory   jsonFactory;
+	private static final Logger LOGGER = LoggerFactory.getLogger(YouTubeProvider.class);
 
 	@Inject
 	public YouTubeProvider(final HttpTransport httpTransport, final JsonFactory jsonFactory) {
@@ -42,6 +48,17 @@ public class YouTubeProvider implements Provider<YouTube> {
 			final Credential credential = new GoogleCredential.Builder().setJsonFactory(jsonFactory)
 					.setTransport(httpTransport)
 					.setClientSecrets(GDATAConfig.CLIENT_ID, GDATAConfig.CLIENT_SECRET)
+					.addRefreshListener(new CredentialRefreshListener() {
+						@Override
+						public void onTokenResponse(final Credential credential, final TokenResponse tokenResponse) throws IOException {
+							LOGGER.info("Token refreshed {}", tokenResponse.toPrettyString());
+						}
+
+						@Override
+						public void onTokenErrorResponse(final Credential credential, final TokenErrorResponse tokenErrorResponse) throws IOException {
+							LOGGER.error("Token refresh error {}", tokenErrorResponse.toPrettyString());
+						}
+					})
 					.build();
 			credential.setRefreshToken(account.getRefreshToken());
 			services.put(account, new YouTube.Builder(httpTransport, jsonFactory, new HttpRequestInitializer() {
--- src/main/java/de/chaosfisch/uploader/ApplicationData.java
+++ src/main/java/de/chaosfisch/uploader/ApplicationData.java
@@ -14,7 +14,7 @@ import com.sun.javafx.PlatformUtil;
 
 public final class ApplicationData {
 	public static final String VERSION = "3.1.0.1";
-	public static final int    RELEASE = 12;
+	public static final int    RELEASE = 13;
 	public static final String BASEURL = "http://youtubeuploader.square7.ch/nightly/jupidator/update.xml";
 	public static final String HOME;
 	public static final String DATA_DIR;
