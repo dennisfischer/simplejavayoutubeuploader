6f036110de2a4233fcaad479e094991f29294e0e
Dennis Fischer <chaosking0912@gmail.com>
2013-12-02 22:33:45 +0100

    Rewritten authentication dialog. 50% finished.

--- src/main/java/de/chaosfisch/uploader/gui/controller/AccountAddDialogController.java
+++ src/main/java/de/chaosfisch/uploader/gui/controller/AccountAddDialogController.java
@@ -16,41 +16,241 @@ import com.google.inject.Inject;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
-import com.sun.webpane.webkit.dom.HTMLAnchorElementImpl;
+import com.sun.webpane.webkit.dom.HTMLInputElementImpl;
 import de.chaosfisch.google.GDATAConfig;
 import de.chaosfisch.google.account.Account;
 import de.chaosfisch.google.account.IAccountService;
 import de.chaosfisch.google.http.PersistentCookieStore;
-import javafx.beans.InvalidationListener;
-import javafx.beans.Observable;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Worker;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.SceneBuilder;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBoxBuilder;
+import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageBuilder;
+import javafx.stage.StageStyle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 
 import java.io.UnsupportedEncodingException;
 import java.net.CookieHandler;
 import java.net.CookieManager;
+import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AccountAddDialogController extends UndecoratedDialogController {
 
 	@FXML
-	public Label   title;
+	private ResourceBundle resources;
+
 	@FXML
-	public WebView webView;
+	private URL location;
+
+	@FXML
+	private PasswordField password;
+
+	@FXML
+	private TextField username;
+
+	@FXML
+	private TextField code;
+
+	@FXML
+	private GridPane step1;
+
+	@FXML
+	private GridPane step2;
+
+	@FXML
+	private VBox step3;
+
+	@FXML
+	private ProgressIndicator loading;
 
 	private final IAccountService accountService;
+	private final WebView webView = new WebView();
+	private int selectedOption;
+
+	{
+		final Scene scene = SceneBuilder.create().root(webView).build();
+		final Stage stage = StageBuilder.create().scene(scene).build();
+		stage.initStyle(StageStyle.DECORATED);
+		stage.initModality(Modality.NONE);
+		stage.requestFocus();
+		stage.show();
+	}
+
+	@FXML
+	void onLogin(final ActionEvent event) {
+		step1.setVisible(false);
+		loading.setVisible(true);
+		final PersistentCookieStore persistentCookieStore = new PersistentCookieStore();
+	/*	if (null != account) {
+			persistentCookieStore.setSerializeableCookies(account.getSerializeableCookies());
+		}   */
+		final CookieManager cmrCookieMan = new CookieManager(persistentCookieStore, null);
+		CookieHandler.setDefault(cmrCookieMan);
+
+		webView.getEngine().getLoadWorker().stateProperty().addListener(new ChangeListener<Worker.State>() {
+			@Override
+			public void changed(final ObservableValue<? extends Worker.State> observableValue, final Worker.State state, final Worker.State state2) {
+				LOGGER.info("Browser at {}", webView.getEngine().getLocation());
+
+				if (Worker.State.SUCCEEDED == state2) {
+					final String location = webView.getEngine().getLocation();
+					if (location.contains("accounts.google.com/ServiceLogin?")) {
+						handleStep1();
+					} else if (location.contains("accounts.google.com/ServiceLoginAuth")) {
+						loading.setVisible(false);
+						step1.setVisible(true);
+					} else if (location.contains("accounts.google.com/SecondFactor")) {
+						loading.setVisible(false);
+						step2.setVisible(true);
+					} else if (location.contains("youtube.com/my_videos")) {
+						webView.getEngine().load("https://www.youtube.com/channel_switcher?next=%2F");
+					} else if (location.contains("youtube.com/channel_switcher")) {
+						handleStep3();
+					} else if (location.endsWith("youtube.com/")) {
+						copyCookies = new ArrayList<>(persistentCookieStore.getSerializeableCookies());
+						persistentCookieStore.removeAll();
+						startOAuthFlow(webView.getEngine());
+					}
+				}
+			}
+		});
+
+		webView.getEngine().titleProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(final ObservableValue<? extends String> observableValue, final String oldTitle, final String newTitle) {
+
+				System.out.println(newTitle);
+				if (null != newTitle) {
+					final Matcher matcher = OAUTH_TITLE_PATTERN.matcher(newTitle);
+					if (matcher.matches()) {
+						try {
+							final Account account = null;
+							final Account modifiedAccount = null != account ? account : new Account();
+							modifiedAccount.setRefreshToken(accountService.getRefreshToken(matcher.group(1)));
+							modifiedAccount.setSerializeableCookies(copyCookies);
+
+							final HttpResponse<JsonNode> response = Unirest.get(USERINFO_URL)
+									.header("Authorization", accountService.getAuthentication(modifiedAccount)
+											.getHeader())
+									.asJson();
+
+							modifiedAccount.setName(response.getBody().getObject().getString("email"));
+							if (!accountService.verifyAccount(modifiedAccount)) {
+								startOAuthFlow(webView.getEngine());
+							} else {
+								if (null != account) {
+									accountService.update(modifiedAccount);
+								} else {
+									accountService.insert(modifiedAccount);
+								}
+								closeDialog(null);
+							}
+						} catch (Exception e) {
+							LOGGER.error("Authentication exception", e);
+						}
+					}
+				}
+			}
+		});
+
+		webView.getEngine().load("http://www.youtube.com/my_videos");
+	}
+
+	@FXML
+	public void continueSecondFactor(final ActionEvent actionEvent) {
+		final Document document = webView.getEngine().getDocument();
+		final HTMLInputElementImpl persistentCookie = (HTMLInputElementImpl) document.getElementById("PersistentCookie");
+		if (null != persistentCookie) {
+			persistentCookie.setChecked(true);
+		}
+		((HTMLInputElementImpl) document.getElementById("smsUserPin")).setValue(code.getText());
+		((HTMLInputElementImpl) document.getElementById("smsVerifyPin")).click();
+		step2.setVisible(false);
+		loading.setVisible(true);
+	}
+
+	private void handleStep3() {
+
+		final WebEngine engine = webView.getEngine();
+		final int length = (int) engine.executeScript("document.getElementsByClassName(\"channel-switcher-button\").length");
+		if (1 < length) {
+			loading.setVisible(false);
+			step3.setVisible(true);
+		} else {
+			webView.getEngine().load("https://www.youtube.com");
+		}
+		for (int i = 0; i < length; i++) {
+			final String name = (String) engine.executeScript("document.evaluate('//*[@id=\"channel-switcher-content\"]/ul/li[" + (i + 1) + "]/a/span/span[2]', document, null, XPathResult.ANY_TYPE, null).iterateNext().innerHTML.trim()");
+			final String image = (String) engine.executeScript("document.evaluate('//*[@id=\"channel-switcher-content\"]/ul/li[" + (i + 1) + "]/a/span/span[1]/span/span/span/img', document, null, XPathResult.ANY_TYPE, null).iterateNext().src");
+			final String url = (String) engine.executeScript("document.evaluate('//*[@id=\"channel-switcher-content\"]/ul/li[" + (i + 1) + "]/a', document, null, XPathResult.ANY_TYPE, null).iterateNext().href");
+
+			final int tmpI = i;
+			step3.getChildren()
+					.add(HBoxBuilder.create()
+							.alignment(Pos.CENTER_LEFT)
+							.children(new ImageView(new Image(image)), new Label(name))
+							.onMouseClicked(new EventHandler<MouseEvent>() {
+								@Override
+								public void handle(final MouseEvent mouseEvent) {
+									step3.setVisible(false);
+									loading.setVisible(true);
+									selectedOption = tmpI;
+									engine.load(url);
+								}
+							})
+							.onMouseEntered(new EventHandler<MouseEvent>() {
+								@Override
+								public void handle(final MouseEvent mouseEvent) {
+									((Node) mouseEvent.getSource()).setCursor(Cursor.HAND);
+								}
+							})
+							.onMouseExited(new EventHandler<MouseEvent>() {
+								@Override
+								public void handle(final MouseEvent mouseEvent) {
+									((Node) mouseEvent.getSource()).setCursor(Cursor.DEFAULT);
+								}
+							})
+							.build());
+		}
+	}
+
+	private void handleStep1() {
+		step1.setVisible(false);
+		loading.setVisible(true);
+		final Document document = webView.getEngine().getDocument();
+		((HTMLInputElementImpl) document.getElementById("Email")).setValue(username.getText());
+		((HTMLInputElementImpl) document.getElementById("Passwd")).setValue(password.getText());
+		((HTMLInputElementImpl) document.getElementById("signIn")).click();
+	}
 
 	@Inject
 	public AccountAddDialogController(final IAccountService accountService) {
@@ -58,9 +258,13 @@ public class AccountAddDialogController extends UndecoratedDialogController {
 	}
 
 	@FXML
-	public void initialize() {
-		assert null != title : "fx:id=\"title\" was not injected: check your FXML file 'AccountAddDialog.fxml'.";
-		assert null != webView : "fx:id=\"title\" was not injected: check your FXML file 'AccountAddDialog.fxml'.";
+	void initialize() {
+		assert null != password : "fx:id=\"password\" was not injected: check your FXML file 'AccountAddDialog.fxml'.";
+		assert null != username : "fx:id=\"username\" was not injected: check your FXML file 'AccountAddDialog.fxml'.";
+		assert null != view : "fx:id=\"view\" was not injected: check your FXML file 'AccountAddDialog.fxml'.";
+		assert null != step1 : "fx:id=\"step1\" was not injected: check your FXML file 'AccountAddDialog.fxml'.";
+		assert null != step2 : "fx:id=\"step2\" was not injected: check your FXML file 'AccountAddDialog.fxml'.";
+		assert null != step3 : "fx:id=\"step3\" was not injected: check your FXML file 'AccountAddDialog.fxml'.";
 	}
 
 	private static final Pattern  OAUTH_TITLE_PATTERN = Pattern.compile("Success code=(.*)");
@@ -72,70 +276,11 @@ public class AccountAddDialogController extends UndecoratedDialogController {
 														 "https://www.googleapis.com/auth/userinfo.email"};
 	private static final String   OAUTH_URL           = "https://accounts.google.com/o/oauth2/auth?access_type=offline&scope=%s&redirect_uri=%s&response_type=%s&client_id=%s";
 	private static final String   USERINFO_URL        = "https://www.googleapis.com/oauth2/v1/userinfo";
-	private static final Logger   logger              = LoggerFactory.getLogger(AccountAddController.class);
+	private static final Logger   LOGGER              = LoggerFactory.getLogger(AccountAddController.class);
+
+	private List<PersistentCookieStore.SerializableCookie> copyCookies;
 
 	public void initWebView(final Account account) {
-		final PersistentCookieStore persistentCookieStore = new PersistentCookieStore();
-		if (null != account) {
-			persistentCookieStore.setSerializeableCookies(account.getSerializeableCookies());
-		}
-		final CookieManager cmrCookieMan = new CookieManager(persistentCookieStore, null);
-		CookieHandler.setDefault(cmrCookieMan);
-
-		final WebEngine webEngine = webView.getEngine();
-		webView.setContextMenuEnabled(false);
-
-		title.setText("Loading...");
-		webView.getEngine().load("http://www.youtube.com/my_videos");
-		webView.getEngine().getLoadWorker().stateProperty().addListener(new ChangeListener<Worker.State>() {
-			@Override
-			public void changed(final ObservableValue<? extends Worker.State> observableValue, final Worker.State state, final Worker.State state2) {
-				logger.info("Browser at {}", webView.getEngine().getLocation());
-
-				if (Worker.State.SUCCEEDED == state2) {
-					if (webView.getEngine().getLocation().contains("youtube.com/my_videos")) {
-						startOAuthFlow(webEngine);
-					}
-				}
-			}
-		});
-
-		webEngine.titleProperty().addListener(new ChangeListener<String>() {
-			@Override
-			public void changed(final ObservableValue<? extends String> observableValue, final String oldTitle, final String newTitle) {
-
-				if (null != newTitle) {
-					final Matcher matcher = OAUTH_TITLE_PATTERN.matcher(newTitle);
-					if (matcher.matches()) {
-						try {
-							final Account modifiedAccount = null != account ? account : new Account();
-							modifiedAccount.setRefreshToken(accountService.getRefreshToken(matcher.group(1)));
-							modifiedAccount.setSerializeableCookies(persistentCookieStore.getSerializeableCookies());
-
-							final HttpResponse<JsonNode> response = Unirest.get(USERINFO_URL)
-									.header("Authorization", accountService.getAuthentication(modifiedAccount)
-											.getHeader())
-									.asJson();
-
-							modifiedAccount.setName(response.getBody().getObject().getString("email"));
-							if (!accountService.verifyAccount(modifiedAccount)) {
-								startOAuthFlow(webEngine);
-							} else {
-								if (null != account) {
-									accountService.update(modifiedAccount);
-								} else {
-									accountService.insert(modifiedAccount);
-								}
-								closeDialog(null);
-							}
-						} catch (Exception e) {
-							logger.error("Authentication exception", e);
-						}
-					}
-				}
-			}
-		});
-		title.textProperty().bind(webEngine.titleProperty());
 	}
 
 	private void startOAuthFlow(final WebEngine webEngine) {
@@ -143,26 +288,6 @@ public class AccountAddDialogController extends UndecoratedDialogController {
 			final Joiner joiner = Joiner.on(" ").skipNulls();
 			final String scope = URLEncoder.encode(joiner.join(SCOPES), Charsets.UTF_8.toString());
 			webEngine.load(String.format(OAUTH_URL, scope, GDATAConfig.REDIRECT_URI, "code", GDATAConfig.CLIENT_ID));
-			webEngine.documentProperty().addListener(new InvalidationListener() {
-				@Override
-				public void invalidated(final Observable observable) {
-					final Document doc = webEngine.getDocument();
-
-					if (null != doc) {
-						final Element accountList = doc.getElementById("account-list");
-
-						if (null != accountList) {
-							final NodeList accounts = accountList.getElementsByTagName("li");
-							if (null != accounts && 1 < accounts.getLength()) {
-								final HTMLAnchorElementImpl button = (HTMLAnchorElementImpl) accounts.item(0)
-										.getChildNodes()
-										.item(1);
-								webEngine.load(button.getHref());
-							}
-						}
-					}
-				}
-			});
 		} catch (UnsupportedEncodingException ignored) {
 		}
 	}
